f3531aa9a53c366c3321f5f77fd6c62b500c9436
oldratlee <oldratlee@gmail.com>
2013-02-10 02:41:13 +0800

    improve javadoc

---
+++ src/test/java/com/metaframe/cooma/ext1/impl/SimpleExtNotConfigedImpl.java
@@ -0,0 +1,22 @@
+package com.metaframe.cooma.ext1.impl;
+
+import com.metaframe.cooma.Config;
+import com.metaframe.cooma.ext1.SimpleExt;
+
+/**
+ * @author Jerry Lee(oldratlee<at>gmail<dot>com)
+ */
+public class SimpleExtNotConfigedImpl implements SimpleExt {
+    public String echo(Config config, String s) {
+        return "Ext1Impl3-echo";
+    }
+
+    public String yell(Config config, String s) {
+        return "Ext1Impl3-yell";
+    }
+
+    public String bang(Config config, int i) {
+        return "bang3";
+    }
+
+}
